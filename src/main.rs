e4ad6ae2e4c979bbe3b2493b496f1b27accf4192
Elias Dilem dos Santos <eliasdilem@gmail.com>
2024-02-20 22:41:28 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate winapi;
 
 mod janela;
 use imagesize::size;
-use std::cmp::Ordering;
+use std::cmp::Ordering::*;
 use std::env;
 
 const RESPOSTA: [&str; 4] = [
@@ -16,53 +16,61 @@ const RESPOSTA: [&str; 4] = [
 
 struct VerTam {
     foto: String,
-    dimensoes: (usize, usize),
-    resposta: &'static str,
+    largura: usize,
+    altura: usize,
+    resposta: &'static str
 }
 
 impl VerTam {
     fn obter_dados() -> Self {
         if env::args().len() != 2 {
-            let _ = janela::rodar_janela("Favor entrar com um arquivo.");
+            janela::rodar_janela("Favor entrar com um arquivo.");
             panic!();
         };
+
         let foto = env::args()
             .nth(1)
             .unwrap()
-            .split("\\")
+            .split('\\')
             .last()
             .unwrap()
             .into();
+
         let dim = size(&foto).unwrap();
+
         Self {
             foto,
-            dimensoes: (dim.width, dim.height),
+            largura: dim.width,
+            altura: dim.height,
             resposta: "",
         }
     }
+
     fn comparar(&mut self) -> &Self {
-        match self.dimensoes.0.cmp(&self.dimensoes.1) {
-            Ordering::Greater => {
-                self.dimensoes = (self.dimensoes.1, self.dimensoes.0);
+        match self.largura.cmp (&self.altura) {
+            Greater => {
+                let temp = self.largura;
+                self.largura = self.altura;
+                self.altura = temp;
                 self.calcular();
             }
-            Ordering::Less => {
-                self.resposta = self.calcular();
-            }
-            Ordering::Equal => self.resposta = "quadrado.",
+            Less => self.resposta = self.calcular(),
+            Equal => self.resposta = "quadrado.",
         }
         self
     }
+
     fn calcular(&self) -> &'static str {
         [2, 3, 4, 5, 7]
             .windows(2) // Forma os PARES: [[2, 3],[3, 4],[4, 5],[5, 7]]
             .enumerate()
-            .find(|f| self.dimensoes.0 as isize / f.1[0] - self.dimensoes.1 as isize / f.1[1] == 0)
+            .find(|f| self.largura / f.1[0] - self.altura / f.1[1] == 0)
             .map_or("desconhecido.", |r| RESPOSTA[r.0])
     }
+
     fn responder(&self) {
         let resposta = format!("O tamanho da foto < {} > é {}", self.foto, self.resposta);
-        let _ = janela::rodar_janela(&resposta);
+        janela::rodar_janela(&resposta);
     }
 }
 
@@ -77,7 +85,8 @@ mod teste {
     fn teste() {
         let mut vertam = VerTam {
             foto: "Foto_A308.jpg".into(),
-            dimensoes: (1200, 1680),
+            largura: 1200,
+            altura: 1680,
             resposta: "",
         };
         vertam.comparar().responder();
